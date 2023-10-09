78fdb18137aa5deb4fcea84fd4b7202d8d89fbe4
Tetiana <kuzmenkotv22@gmail.com>
2023-10-08 20:36:31 -0600

    portfolio block was done

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,19 @@ const closeMenu = document.querySelector(".menu__close");
 
 hamburger.addEventListener("click", () => {
 	menu.classList.add("active");
+	window.onscroll = () => {
+		window.scroll(0, 0);
+	};
 });
 
 closeMenu.addEventListener("click", () => {
 	menu.classList.remove("active");
+	window.onscroll = null;
 });
 
 menu.addEventListener("click", function (e) {
 	if (!e.target.closest(".menu__block")) {
 		menu.classList.remove("active");
+		window.onscroll = null;
 	}
 });
