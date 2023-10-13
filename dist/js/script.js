979a2dca3e82eadc9d506a3aa19c1165a9330f16
Tetiana <kuzmenkotv22@gmail.com>
2023-10-13 13:09:07 -0600

    added user notification about cookies

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,3 +20,21 @@ menu.addEventListener("click", function (e) {
 		window.onscroll = null;
 	}
 });
+
+const cookieBox = document.querySelector(".cookies__wrapper"),
+	acceptBtn = cookieBox.querySelector(".cookies_item");
+
+acceptBtn.onclick = () => {
+	//setting cookie for 1 month, after one month it'll be expired automatically
+	document.cookie = "CookieBy=CodingNepal; max-age=" + 60 * 60 * 24 * 30;
+	if (document.cookie) {
+		//if cookie is set
+		cookieBox.classList.add("hide"); //hide cookie box
+	} else {
+		//if cookie not set then alert an error
+		alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
+	}
+};
+let checkCookie = document.cookie.indexOf("CookieBy=CodingNepal"); //checking our cookie
+//if cookie is set then hide the cookie box else show it
+checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
