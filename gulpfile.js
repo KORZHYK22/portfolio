acaa1978f4129b93e942df95eea5a9f28e4bf07a
Tetiana <kuzmenkotv22@gmail.com>
2023-10-14 19:33:45 -0600

    adaptability

--- gulpfile.js
+++ gulpfile.js
@@ -44,43 +44,19 @@ gulp.task("html", function () {
 });
 
 gulp.task("scripts", function () {
-	return gulp
-		.src("src/js/**/*.js")
-		.pipe(gulp.dest("dist/js"))
-		.pipe(browserSync.stream());
+	return gulp.src("src/js/**/*.js").pipe(gulp.dest("dist/js")).pipe(browserSync.stream());
 });
 
 gulp.task("fonts", function () {
-	return gulp
-		.src("src/fonts/**/*")
-		.pipe(gulp.dest("dist/fonts"))
-		.pipe(browserSync.stream());
+	return gulp.src("src/fonts/**/*").pipe(gulp.dest("dist/fonts")).pipe(browserSync.stream());
 });
 
 gulp.task("icons", function () {
-	return gulp
-		.src("src/icons/**/*")
-		.pipe(gulp.dest("dist/icons"))
-		.pipe(browserSync.stream());
+	return gulp.src("src/icons/**/*").pipe(gulp.dest("dist/icons")).pipe(browserSync.stream());
 });
 
 gulp.task("images", function () {
-	return gulp
-		.src("src/img/**/*")
-		.pipe(gulp.dest("dist/img"))
-		.pipe(browserSync.stream());
+	return gulp.src("src/img/**/*").pipe(gulp.dest("dist/img")).pipe(browserSync.stream());
 });
 
-gulp.task(
-	"default",
-	gulp.parallel(
-		"watch",
-		"server",
-		"styles",
-		"scripts",
-		"fonts",
-		"icons",
-		"html",
-		"images"
-	)
-);
+gulp.task("default", gulp.parallel("watch", "server", "styles", "scripts", "fonts", "icons", "html", "images"));
