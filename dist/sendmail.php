1969195e3067764173a8acae37b14cf2b1f3993f
Tetiana <kuzmenkotv22@gmail.com>
2023-10-20 14:19:11 -0600

    phpmailer updated

--- dist/sendmail.php
+++ dist/sendmail.php
@@ -4,52 +4,48 @@
 
 	require 'phpmailer/src/Exception.php';
 	require 'phpmailer/src/PHPMailer.php';
-	
+	require 'phpmailer/src/SMTP.php';
 
 	$mail = new PHPMailer(true);
 	$mail->CharSet = 'UTF-8';
+	$mail->Subject = 'I would like to contact with you!';
 	$mail->setLanguage('en', 'phpmailer/language/');
 	$mail->IsHTML(true);
 
+	try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'test@gmail.com';                     //SMTP username
+    $mail->Password   = 'password';                               //SMTP password
+    $mail->SMTPSecure = 'TLS';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-
-	$mail->setFrom('kuzmenkotv1722@gmail.com'); 
-
-	$mail->addAddress('kuzmenkotv22@gmail.com'); 
-	
-	$mail->Subject = 'Tetiana Kuzmenko | Software Developer';
-
-	
-	$body = '<h1> I would like to contact with you! </h1>';
+    //Recipients
+    $mail->setFrom('test@gmail.com', 'Tetiana Kuzmenko | Developer ');
+    $mail->addAddress('test@gmail.com', 'Tetiana Kuzmenko');     //Add a recipient
+   
+    $body = '<h2>New contact</h2>';
 
 	if(trim(!empty($_POST['name']))){
-		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+		$body.='<p><strong>Name:</strong> '.$_POST['name'].'</p>';
 	}
 	if(trim(!empty($_POST['email']))){
 		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
 	}
-	if(trim(!empty($_POST['hand']))){
-		$body.='<p><strong>Рука:</strong> '.$hand.'</p>';
-	}
-	if(trim(!empty($_POST['age']))){
-		$body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
-	}
 	
-	if(trim(!empty($_POST['message']))){
-		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	if(trim(!empty($_POST['text']))){
+		$body.='<p><strong>Message:</strong> '.$_POST['text'].'</p>';
 	}
 
 	$mail->Body = $body;
+    //Content
+                                  
 
-	//Відправляємо
-	if (!$mail->send()) {
-		$message = 'Error';
-	} else {
-		$message = 'Data sent!';
-	}
 
-	$response = ['message' => $message];
-
-	header('Content-type: application/json');
-	echo json_encode($response);
-?>
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
