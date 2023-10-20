b98a71876cc044861850dd315ca6c66bee5b1fc3
Tetiana <kuzmenkotv22@gmail.com>
2023-10-20 08:55:33 -0600

    Updates

---
+++ sendmail.php
@@ -0,0 +1,55 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+	
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('en', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+
+
+	$mail->setFrom('test@gmail.com'); 
+
+	$mail->addAddress('kuzmenkotv22@gmail.com'); 
+	
+	$mail->Subject = 'Tetiana Kuzmenko | Software Developer';
+
+	
+	$body = '<h1> I would like to contact with you! </h1>';
+
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['hand']))){
+		$body.='<p><strong>Рука:</strong> '.$hand.'</p>';
+	}
+	if(trim(!empty($_POST['age']))){
+		$body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
+	}
+	
+	if(trim(!empty($_POST['message']))){
+		$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+	}
+
+	$mail->Body = $body;
+
+	//Відправляємо
+	if (!$mail->send()) {
+		$message = 'Error';
+	} else {
+		$message = 'Data sent!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
